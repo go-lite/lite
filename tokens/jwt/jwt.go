84f17f69f598ea91e8ab78a359c4507552e5f885
Drissa KONE <koneenok@outlook.fr>
2024-07-12 12:51:10 +0200

    fix datetime generation in openapi

--- tokens/jwt/jwt.go
+++ tokens/jwt/jwt.go
@@ -18,18 +18,25 @@ func Parse[T any](token string) (T, error) {
 
 	payload, err := base64.RawURLEncoding.DecodeString(parts[1])
 	if err != nil {
-		return result, fmt.Errorf("failed to decode token payload: %v", err)
+		return result, fmt.Errorf("failed to decode token payload: %w", err)
 	}
 
 	err = json.Unmarshal(payload, &result)
 	if err != nil {
-		return result, fmt.Errorf("failed to unmarshal token payload: %v", err)
+		return result, fmt.Errorf(
+			"failed to unmarshal token payload: %w",
+			err,
+		)
 	}
 
 	var payloadMap map[string]interface{}
+
 	err = json.Unmarshal(payload, &payloadMap)
 	if err != nil {
-		return result, fmt.Errorf("failed to unmarshal token payload for expiration check: %v", err)
+		return result, fmt.Errorf(
+			"failed to unmarshal token payload for expiration check: %w",
+			err,
+		)
 	}
 
 	if exp, ok := payloadMap["exp"].(float64); ok {
--- tokens/paseto/paseto.go
+++ tokens/paseto/paseto.go
@@ -14,9 +14,10 @@ func Parse[T any](token string, key []byte) (T, error) {
 	var footer string
 
 	v2 := paseto.NewV2()
+
 	err := v2.Decrypt(token, key, &jsonToken, &footer)
 	if err != nil {
-		return result, fmt.Errorf("failed to decrypt token: %v", err)
+		return result, fmt.Errorf("failed to decrypt token: %w", err)
 	}
 
 	if jsonToken.Expiration.Before(time.Now()) {
@@ -25,12 +26,12 @@ func Parse[T any](token string, key []byte) (T, error) {
 
 	payload, err := json.Marshal(jsonToken)
 	if err != nil {
-		return result, fmt.Errorf("failed to marshal token payload: %v", err)
+		return result, fmt.Errorf("failed to marshal token payload: %w", err)
 	}
 
 	err = json.Unmarshal(payload, &result)
 	if err != nil {
-		return result, fmt.Errorf("failed to unmarshal token payload: %v", err)
+		return result, fmt.Errorf("failed to unmarshal token payload: %w", err)
 	}
 
 	return result, nil
